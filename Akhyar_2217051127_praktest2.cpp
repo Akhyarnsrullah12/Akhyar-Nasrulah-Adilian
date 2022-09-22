a49a5860c679339101e8fc2f9c324edf2f73cfdb
Akhyarnsrullah12 <112605337+Akhyarnsrullah12@users.noreply.github.com>
2022-09-22 19:33:54 +0700

    Add files via upload

---
+++ Akhyar_2217051127_praktest2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+int nilai;
+cin>>nilai;
+if(nilai==1){
+	cout<<"satu\n";
+}else if(nilai==2){
+	cout<<"dua\n";
+	}else if(nilai==3){
+	cout<<"tiga\n";
+	cout<<"bilangan lebih besar dari tiga\n";
+    }
+}
