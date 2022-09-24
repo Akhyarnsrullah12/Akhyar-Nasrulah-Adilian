dcf564632a0ad4bb747b4ba7fb65195fdff21e07
Akhyarnsrullah12 <112605337+Akhyarnsrullah12@users.noreply.github.com>
2022-09-24 20:27:49 +0700

    Add files via upload

---
+++ 2217051127_Akhyar_Postest1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"TIPE DATA"<<endl;
+	cout<<".........................."<<endl;
+	
+	int A = 19;
+	float B = 17.1;
+	string C = "Akhyar Nasrullah";
+	char D = 'A';
+	
+	cout<<"Umur : "<<A<<endl;
+	cout<<"Tinggi Badan : "<<B<<endl;
+	cout<<"Nama : "<<C<<endl;
+	cout<<"Gol Darah : "<<D<<endl;	
+}
